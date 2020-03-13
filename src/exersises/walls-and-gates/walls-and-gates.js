ed8b7d4f5680a6f4f568640ce1523d73a8efd5a6
Kirill Shatskiy <shaackiy@gmail.com>
2020-03-13 10:29:48 +0300

    feat: walls-and-gates first implementation

--- src/exersises/walls-and-gates/walls-and-gates.js
+++ src/exersises/walls-and-gates/walls-and-gates.js
@@ -13,6 +13,7 @@ INF  -1  0  INF
 INF INF INF  -1
 INF  -1 INF  -1
   0  -1 INF INF
+
 After running your function, the 2D grid should be:
 
   3  -1   0   1
@@ -20,35 +21,89 @@ After running your function, the 2D grid should be:
   1  -1   2  -1
   0  -1   3   4 */
 
+var INF = 2147483647;
+var WALL = -1;
+var GATE = 0;
+
+function Node(value, row, column) {
+    this.value = value;
+    this.row = row;
+    this.column = column
+    this.neighbors = [];
+}
+
+function getNeighbors(node, grid) {
+    const neighbors = [];
+    if (node.row > 0) {
+        neighbors.push(grid[node.row - 1][node.column]);
+    }
+
+    if (node.row < grid.length - 1) {
+        neighbors.push(grid[node.row + 1][node.column]);
+    }
+
+    if (node.column > 0) {
+        neighbors.push(grid[node.row][node.column - 1])
+    }
+
+    if (node.column < grid[0].length - 1) {
+        neighbors.push(grid[node.row][node.column + 1])
+    }
+
+
+    return neighbors;
+}
+
 /**
  * @param {number[][]} rooms
  * @return {[Nodes[], Node[]]}
  */
-function createGraph() {
+function createGraph(rooms) {
     // map grid to Node[][]
-    const nodes = rooms.map((row) => {
-        return row.map((cell) => createNode(cell))
+    const nodes = rooms.map((row, rowIndex) => {
+        return row.map((cell, cellIndex) => new Node(cell, rowIndex, cellIndex))
     })
-    const listOfNodes = [];
     const listOfRooms = [];
-    nodes.forEach((row, rowIndex) => {
-        row.forEach((cell, cellIndex) => {
+    const listOfNodes = [];
 
+    nodes.forEach((row) => {
+        row.forEach((node) => {
+            if (node.value !== WALL) {
+                listOfNodes.push(node);
+                node.neighbors = getNeighbors(node, nodes);
+            }
+            if (node.value === INF) {
+                listOfRooms.push(node);
+            }
         });
     });
-    // for each node 
-        // add neighbors
-        // put emprty rooms in a separate array
-        // put node into an arry
-    // return array of nodes and array of empty rooms
+    return listOfRooms;
 }
 
 /**
  * @param {Node[]} nodes
  * @param {Node[]} room
  */
-function findDistanceToGate() {
-    // Use BFS to find shortest distance to a gate
+function findDistanceToGate(room) {
+    const queue = [room];
+    let step = 0;
+
+    while (queue.length > 0) {
+        const size = queue.length;
+        for (let i = 0; i < size; i++) {
+            const cur = queue[0];
+            if (cur.value === GATE) {
+                return step;
+            }
+            cur.neighbors.forEach((node) => {
+                queue.push(node)
+            });
+            queue.shift();
+        }
+        step += 1;
+    }
+
+    return INF;
 }
 
 /**
@@ -57,14 +112,15 @@ function findDistanceToGate() {
  */
 function wallsAndGates(rooms) {
     // create the graph
-    const [nodes, emptyRooms] = createGraph(rooms);
+    const emptyRooms = createGraph(rooms);
     // for every room
     emptyRooms.forEach((room) => {
         // find shortest path to the nearest gate
-        const distanceToGate = findDistanceToGate(nodes, room);
+        const distanceToGate = findDistanceToGate(room);
         // modify value in array
-        rooms[room.row][room.col] = distanceToGate;
+        rooms[room.row][room.column] = distanceToGate;
     });
+    console.log(rooms);
 }
 
 module.exports = wallsAndGates;
--- src/exersises/walls-and-gates/walls-and-gates.spec.js
+++ src/exersises/walls-and-gates/walls-and-gates.spec.js
@@ -1,5 +1,24 @@
 const wallsAndGates = require('./walls-and-gates');
 
+const INF = 2147483647;
+
 describe('wallsAndGates', () => {
-    
+    it('will print', () => {
+        const input = [
+            [INF, -1, 0, INF],
+            [INF, INF, INF, -1],
+            [INF, -1, INF, -1],
+            [0, -1, INF, INF]
+        ];
+
+        wallsAndGates(input);
+
+        const expected = [
+            [3, -1, 0,  1],
+            [2,  2, 1, -1],
+            [1, -1, 2, -1],
+            [0, -1, 3,  4],
+        ];
+        expect(input).toEqual(expected)
+    })
 });
