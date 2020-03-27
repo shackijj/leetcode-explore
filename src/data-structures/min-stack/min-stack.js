7092f5c04c6a61a3c31b45bb83ce0edcb499604b
Kirill Shatskiy <shaackiy@gmail.com>
2020-03-27 09:49:25 +0300

    feat: simplify MinStack by removing .history

--- src/data-structures/min-stack/min-stack.js
+++ src/data-structures/min-stack/min-stack.js
@@ -1,48 +1,40 @@
+function last(array) {
+    return array[array.length - 1];
+}
+
 class MinStack {
     constructor() {
         this.mins = [];
         this.nums = [];
-        this.history = [];
     }
     /** Push element x onto stack. */
     push(el) {
-        if (this.mins.length === 0 || this.mins[this.mins.length - 1] > el) {
+        this.nums.push(el);
+
+        if (last(this.mins) === undefined || el <= last(this.mins)) {
             this.mins.push(el);
-            this.history.push(0);
-        } else {
-            this.nums.push(el);
-            this.history.push(1)
         }
     }
     /**  Removes the element on top of the stack. */
     pop() {
-        let el;
-        let h = this.history.pop();
-        if (h === 0) {
-            el = this.mins.pop();
-        } else {
-            el = this.nums.pop();
+        const el = this.nums.pop();
+        if (el === last(this.mins)) {
+            this.mins.pop();
         }
-
-        if (this.nums[this.nums.length - 1] < this.mins[this.mins.length - 1]) {
-            this.history[this.history.length - 1] = 0;
-            this.mins.push(this.nums.pop());
-        }
-
         return el;
     }
     /** Get the top element */
     top() {
-        if (this.history[this.history.length - 1] === 0) {
-            return this.getMin();
-        } else {
-            return this.nums[this.nums.length - 1];   
-        }
+        return last(this.nums);
     }
     /** Retrieve the minimum element in the stack. */
     getMin() {
-        return this.mins[this.mins.length - 1];
+        return last(this.mins);
     }
 }
 
+
+/**
+ * Can we do better? (Better applies not only to mem/time complexity, but also simplicity)
+ */
 module.exports = MinStack;
