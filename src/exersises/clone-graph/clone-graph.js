617c83db3aac60dfb832ceefa227556dcc0de48a
Kirill Shatskiy <shaackiy@gmail.com>
2020-04-01 13:15:29 +0300

    feat: implement clone graph

--- src/exersises/clone-graph/clone-graph.js
+++ src/exersises/clone-graph/clone-graph.js
@@ -1,14 +1,31 @@
 function Node(val, neighbors) {
     this.val = val === undefined ? 0 : val;
     this.neighbors = neighbors === undefined ? [] : neighbors;
- }
- 
+}
+
+function DFS(node, map) {
+    const copy = new Node(node.val);
+    map.set(node, copy);
+
+    node.neighbors.forEach((n) => {
+        const visited = map.get(n);
+        if (!visited) {
+            nCopy = DFS(n, map, copy);
+            copy.neighbors.push(nCopy);
+        } else {
+            copy.neighbors.push(visited);
+        }
+    });
+    return copy;
+}
+
 function cloneGraph(node) {
-    const v4 = new Node(4, []);
-    const v3 = new Node(3, []);
-    const v2 = new Node(2, []); 
-    const v1 = new Node(1, [v2, v3, v4]);
-    return v1;
+    if (node === null) {
+        return null;
+    }
+    const map = new Map();
+    DFS(node, map);
+    return map.get(node);
 }
 
 module.exports = {Node, cloneGraph};
--- src/exersises/clone-graph/clone-graph.spec.js
+++ src/exersises/clone-graph/clone-graph.spec.js
@@ -2,14 +2,192 @@ const {cloneGraph, Node} = require('./clone-graph');
 
 describe('cloneGraph', () => {
     it('case 1', () => {
-        const v4 = new Node(4, []);
-        const v3 = new Node(3, []);
         const v2 = new Node(2, []); 
-        const v1 = new Node(1, [v2, v3, v4]);
+        const v1 = new Node(1, [v2]);
+
+        v2.neighbors.push(v1);
 
         const copy = cloneGraph(v1);
 
-        expect(copy).toEqual(v1);
+        expect(copy).toEqual(
+            expect.objectContaining({
+                val: 1,
+                neighbors: [
+                    expect.objectContaining({
+                        val: 2,
+                        neighbors: [
+                            expect.objectContaining({
+                                val: 1
+                            })
+                        ],
+                    }),
+                ]
+            })
+        );
+        expect(copy).not.toBe(v1);
+    });
+
+    it('case 2', () => {
+        const v3 = new Node(3, []);
+        const v2 = new Node(2, []); 
+        const v1 = new Node(1, [v2, v3]);
+
+        v2.neighbors.push(v1);
+        v3.neighbors.push(v1);
+
+        const copy = cloneGraph(v1);
+
+        expect(copy).toEqual(
+            expect.objectContaining({
+                val: 1,
+                neighbors: [
+                    expect.objectContaining({
+                        val: 2,
+                        neighbors: [
+                            expect.objectContaining({
+                                val: 1
+                            }),
+                        ],
+                    }),
+                    expect.objectContaining({
+                        val: 3,
+                        neighbors: [
+                            expect.objectContaining({
+                                val: 1
+                            }),
+                        ],
+                    }),
+                ]
+            })
+        );
+        expect(copy).not.toBe(v1);
+    });
+
+    it('case 2', () => {
+        const v3 = new Node(3, []);
+        const v2 = new Node(2, []); 
+        const v1 = new Node(1, [v2, v3]);
+
+        v2.neighbors.push(v1);
+        v2.neighbors.push(v3);
+        v3.neighbors.push(v2);
+        v3.neighbors.push(v1);
+
+        const copy = cloneGraph(v1);
+
+        expect(copy).toEqual(
+            expect.objectContaining({
+                val: 1,
+                neighbors: [
+                    expect.objectContaining({
+                        val: 2,
+                        neighbors: [
+                            expect.objectContaining({
+                                val: 1
+                            }),
+                            expect.objectContaining({
+                                val: 3
+                            }),
+                        ],
+                    }),
+                    expect.objectContaining({
+                        val: 3,
+                        neighbors: [
+                            expect.objectContaining({
+                                val: 2
+                            }),
+                            expect.objectContaining({
+                                val: 1
+                            }),
+                        ],
+                    }),
+                ]
+            })
+        );
+        expect(copy).not.toBe(v1);
+    });
+
+    it('case 3', () => {
+        const v1 = new Node(1, []);
+        const copy = cloneGraph(v1);
+        expect(copy).toEqual(copy);
+        expect(copy).not.toBe(v1);
+    });
+
+    it('case 4', () => {
+        const v5 = new Node(5, []);
+        const v4 = new Node(4, []);
+        const v3 = new Node(3, []);
+        const v2 = new Node(2, []); 
+        const v1 = new Node(1, [v2, v3]);
+
+        v2.neighbors.push(v1);
+        v2.neighbors.push(v3);
+        v2.neighbors.push(v4);
+
+        v3.neighbors.push(v2);
+        v3.neighbors.push(v5);
+        v3.neighbors.push(v1);
+
+        v4.neighbors.push(v2);
+        v4.neighbors.push(v5);
+
+        v5.neighbors.push(v4);
+        v5.neighbors.push(v3);
+
+        const copy = cloneGraph(v1);
+
+        expect(copy).toEqual(
+            expect.objectContaining({
+                val: 1,
+                neighbors: [
+                    expect.objectContaining({
+                        val: 2,
+                        neighbors: [
+                            expect.objectContaining({
+                                val: 1
+                            }),
+                            expect.objectContaining({
+                                val: 3
+                            }),
+                            expect.objectContaining({
+                                val: 4,
+                                neighbors: [
+                                    expect.objectContaining({
+                                        val: 2
+                                    }),
+                                    expect.objectContaining({
+                                        val: 5,
+                                        neighbors: [
+                                            expect.objectContaining({
+                                                val: 4
+                                            }),
+                                            expect.objectContaining({
+                                                val: 3
+                                            }),
+                                        ],
+                                    }),
+                                ],
+                            }),
+                        ],
+                    }),
+                    expect.objectContaining({
+                        val: 3,
+                        neighbors: [
+                            expect.objectContaining({
+                                val: 2
+                            }),
+                            expect.objectContaining({
+                                val: 5
+                            }),
+                            expect.objectContaining({
+                                val: 1
+                            }),
+                        ],
+                    }),
+                ]
+            })
+        );
         expect(copy).not.toBe(v1);
     });
 });
