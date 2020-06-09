74b83e57c7969accac1df31b0ff5a318f1567ae7
Kirill Shatskiy <shaackiy@gmail.com>
2020-06-09 14:13:00 +0300

    implement decode_string.py

--- python/src/decode_string/decode_string.py
+++ python/src/decode_string/decode_string.py
@@ -1,3 +1,29 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        return "aaabcbc"
+        repeates = []
+        groups = []
+        result = ""
+        num = ""
+        numbers = "0123456789"
+
+        for char in s:
+            if char in numbers:
+                num += char
+            elif char == "[":
+                repeates.append(int(num))
+                groups.append("")
+                num = ""
+            elif char == "]":
+                repeat = repeates.pop()
+                group = groups.pop()
+                if len(repeates) == 0:
+                    result += group * repeat
+                else:
+                    groups[-1] = groups[-1] + (group * repeat)
+            else:
+                if len(repeates) == 0:
+                    result += char
+                else:
+                    groups[-1] = groups[-1] + char
+
+        return result
--- python/src/decode_string/test_decode_string.py
+++ python/src/decode_string/test_decode_string.py
@@ -3,4 +3,12 @@ from decode_string import Solution
 s = Solution()
 
 def test_case0():
+    assert s.decodeString("10[a2[c]]") == "accaccaccaccaccaccaccaccaccacc"
+def test_case1():
+    assert s.decodeString("3[a2[c]]") == "accaccacc"
+def test_case2():
     assert s.decodeString("3[a]2[bc]") == "aaabcbc"
+def test_case3():
+    assert s.decodeString("2[abc]3[cd]ef") == "abcabccdcdcdef"
+def test_case4():
+    assert s.decodeString("abc3[cd]xyz") == "abccdcdcdxyz"
